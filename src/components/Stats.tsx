78dc020ebe59a26369a121c7645cf71e16c5711d
Brayan Fernandez <151949132+Blaaaxo@users.noreply.github.com>
2024-12-20 23:57:54 -0500

    ultimos cambios hero

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -23,7 +23,7 @@ const stats = [
 
 export default function Stats() {
     return (
-        <section>
+        <section className='pt-4 pb-12 xl:pt-0 xl:0b-0'>
             <div className='container mx-auto'>
                 <div className='flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none'>
                     {stats.map((stat, index) => (
@@ -38,7 +38,7 @@ export default function Stats() {
                             />
                             <p className={`${
                                 stat.text.length < 15 ? 'max-w-[100px]': 'max-w-[150px]'
-                            } leading-snug text-white`}>{stat.text}</p>
+                            } leading-snug text-white/80`}>{stat.text}</p>
                         </div>
                     ))}
                 </div>

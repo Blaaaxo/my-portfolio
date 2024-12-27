c5ef55d26f522a7cb9304957de52866fdd644560
Brayan Fernandez <151949132+Blaaaxo@users.noreply.github.com>
2024-12-27 15:50:41 -0500

    formulario contacto funcional

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,12 +1,27 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { motion } from 'framer-motion'
+import { useForm, ValidationError } from '@formspree/react';
 
 
 function Contacto() {
+
+  const [state, handleSubmit] = useForm("xwkgyrep");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (state.succeeded) {
+      setIsModalOpen(true);
+    }
+  }, [state.succeeded]);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -20,7 +35,7 @@ function Contacto() {
         <div className='flex flex-col xl:flex-row gap-[30px]'>
           {/* form */}
           <div className='xl:h-[54%] order-2 xl:order-none'>
-            <form className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
+            <form action={handleSubmit} className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
               <h3 className='text-4xl text-accent text-center'>¡Trabajemos Juntos!😁</h3>
               {/* descripción del formulario */}
               <p className='text-white/60'>
@@ -32,37 +47,58 @@ function Contacto() {
                 <Input
                   className='w-full'
                   type='text'
-                  name='nombre'
+                  name='name'
                   placeholder='Nombre'
                 />
+                <ValidationError
+                  prefix="Name"
+                  field="name"
+                  errors={state.errors}
+                />
                 <Input
-                className='w-full'
+                  className='w-full'
                   type='text'
                   name='email'
                   placeholder='email@example.com'
                 />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                />
                 <Input
-                className='w-full'
+                  className='w-full'
                   type='text'
-                  name='asunto'
+                  name='subject'
                   placeholder='Asunto'
+                />
+                <ValidationError
+                  prefix="Subject"
+                  field="Subject"
+                  errors={state.errors}
                 />
                 <Textarea
                   className='h-[200px]'
-                  name='mensaje'
+                  name='message'
                   placeholder='Escribe tu mensaje aquí...'
+                />
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
                 />
               </div>
               {/* button */}
               <Button
                 size={"md"}
                 type='submit'
+                disabled={state.submitting}
                 className='max-w-40'>
-                Enviar mensaje
+                {state.submitting ? 'Enviando...' : 'Enviar mensaje'}
               </Button>
             </form>
           </div>
-          
+
           {/* imagen con movimiento */}
           <div className='xl:h-[54%] order-1 xl:order-none xl:mt-24 flex justify-center items-center'>
             <motion.img
@@ -81,6 +117,21 @@ function Contacto() {
           </div>
         </div>
       </div>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+          <div className="relative bg-primary p-8 rounded-lg shadow-lg z-10 text-white">
+            <h2 className="text-xl font-bold mb-4">Mensaje enviado</h2>
+            <p>Tu mensaje ha sido enviado exitosamente.</p>
+            <button
+              onClick={closeModal}
+              className="mt-4 bg-accent text-primary py-2 px-4 rounded-full"
+            >
+              Cerrar
+            </button>
+          </div>
+        </div>
+      )}
     </motion.div>
   )
 }
